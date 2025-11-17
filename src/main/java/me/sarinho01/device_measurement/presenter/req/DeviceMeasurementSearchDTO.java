f1d7c67cf3fb45454e60a2f74aa2abccf17f68c9
Sarinho01 <alanvictor.sarinho@gmail.com>
2025-11-16 21:35:10 -0300

    fix: added hibernate validations in search device measurement response presenter

--- src/main/java/me/sarinho01/device_measurement/presenter/req/DeviceMeasurementSearchDTO.java
+++ src/main/java/me/sarinho01/device_measurement/presenter/req/DeviceMeasurementSearchDTO.java
@@ -1,7 +1,13 @@
 package me.sarinho01.device_measurement.presenter.req;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+
 public record DeviceMeasurementSearchDTO(
+        @Min(value = 0, message = "Page cannot be negative")
         Integer page,
+
+        @Positive(message = "Device ID must be greater than 0")
         Long deviceId
 ) {
 }
